a20c47e97469b5b8fc88c47f8169c1865d736e77
WhimsicalNose03 <44480908+WhimsicalNose03@users.noreply.github.com>
2019-04-09 10:22:28 -0700

    convert to function

--- port_scrape.py
+++ port_scrape.py
@@ -1,102 +1,65 @@
 import warnings
 warnings.filterwarnings('ignore')
-
 import copy
 import pandas as pd
-
-# Requests sends and recieves HTTP requests.
 import requests
-
-# Beautiful Soup parses HTML documents in python.
 from bs4 import BeautifulSoup
-
-# imports related to MongoDB
 from pymongo import MongoClient
 import pprint
 
-# Send a GET request for the data
-port_crime_url = 'http://www.city-data.com/crime/crime-Portland-Oregon.html'
-r = requests.get(port_crime_url)
+def scraper(url, mongo_path, tag, column_list):
+    '''
+    We're going to scrape some crime data from a site.
+    
+    Inputs:
+    url: string of url to the webpage that our table is on
+    mongo_path: string with the path to our Mongo DB.  ex: 'mongodb://localhost:27017'
+    tag: string of div tag for the table.  ex: 'table-responsive'
+    column_list: a list of columns in the table we're scraping in the order they appear
+    
+    Output: 
+    a Pandas DF
+    '''
+    
+    # send a GET request and store content 
+    r = requests.get(url)
+    client = MongoClient(mongo_path)
+    db = client.metroid # can be set to any client.
+    pages = db.pages
+    pages.insert_one({'html': r.content})
 
-# check to see if it worked; 200 is good
-r.status_code
+    # use Beautiful Soup to parse HTML to Python Object
+    soup = BeautifulSoup(r.content, 'lxml')
+    soup.prettify()
+    
+    # sift through our data and pull the table we want
+    div = soup.find('div', {'class':tag})
+    table = div.find('table')
+    
+    # create an iterator and accumulator to go over each row in the table
+    rows = table.find_all('tr')
+    all_rows = []
 
-# check our raw hypertext
-r.content
+    # store each column as a dictionary
+    empty_row = {}
+    for i in column_list:
+        empty_row[i] = None
+    empty_row
 
-# save hypertext into mongo for use later
-client = MongoClient('mongodb://localhost:27017/')
-db = client.portcrime
-pages = db.pages
-
-pages.insert_one({'html': r.content})
-
-# Parse html into a Python object
-soup = BeautifulSoup(r.content,'lxml')
-print(soup.prettify())
-
-# navigate through data and pull out the table we are looking for
-div = soup.find('div', {'class':'table-responsive'})
-table = div.find('table')
-
-# create an iterator to go over each row in the table
-rows = table.find_all('tr')
-
-# an empty accumulator 
-all_rows = []
-
-# store each column as a dictionary
-empty_row = {
-    'type': None,
-    '2003': None,
-    '2004': None,
-    '2005': None,
-    '2006': None,
-    '2007': None,
-    '2008': None,
-    '2009': None,
-    '2010': None,
-    '2011': None,
-    '2012': None,
-    '2013': None,
-    '2014': None,
-    '2016': None,
-    '2017': None
-}
-
-# fill our columns, but skip the first row of headers
-for row in rows[1:]:
-    new_row = copy.copy(empty_row)
-    # A list of all entries in the row
-    columns = row.find_all('td')
-    new_row['type'] = columns[0].text.strip()
-    new_row['2003'] = columns[1].text.strip()
-    new_row['2004'] = columns[2].text.strip()
-    new_row['2005'] = columns[3].text.strip()
-    new_row['2006'] = columns[4].text.strip()
-    new_row['2007'] = columns[5].text.strip()
-    new_row['2008'] = columns[6].text.strip()
-    new_row['2009'] = columns[7].text.strip()
-    new_row['2010'] = columns[8].text.strip()
-    new_row['2011'] = columns[9].text.strip()
-    new_row['2013'] = columns[10].text.strip()
-    new_row['2012'] = columns[11].text.strip()
-    new_row['2014'] = columns[12].text.strip()
-    new_row['2016'] = columns[13].text.strip()
-    new_row['2017'] = columns[14].text.strip()
-    all_rows.append(new_row)
-
-# load the rows into our Mongo DB
-port_crime = db.port_crime
-for row in all_rows:
-    port_crime.insert_one(row)
-
-# load rows from Mongo to check
-rows = port_crime.find()
-portland_crime = pd.DataFrame(list(rows))
-portland_crime.head()
-
-# rearrange some columns
-portland_crime = portland_crime.drop('_id',axis=1)
-portland_crime = portland_crime.set_index('type')
-portland_crime.head()
+    # fill our column dictionary, but skip headers
+    for row in rows[1:]:
+        new_row = copy.copy(empty_row)
+        columns = row.find_all('td')
+        for i, row in enumerate(column_list):
+            new_row[row] = columns[i].text.strip()
+        all_rows.append(new_row)
+    
+    # load rows into Mongo DB
+    crime_rate = db.crime_rate
+    for row in all_rows:
+        crime_rate.insert_one(row)
+    
+    # load rows into Pandas DF
+    rows = crime_rate.find()
+    crime_rates = pd.DataFrame(list(rows))
+    return crime_rates.head()
